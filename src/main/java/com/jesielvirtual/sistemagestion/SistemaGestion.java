1a6abf98c37536b796f543fa29efd4c5f12d9234
Jesiel Palacios <jopalacioss@unac.edu.co>
2023-09-01 00:34:37 -0500

    Adds latest changes

---
+++ src/main/java/com/jesielvirtual/sistemagestion/SistemaGestion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jesielvirtual.sistemagestion;
+
+/**
+ *
+ * @author jesielpalacios
+ */
+public class SistemaGestion {
+
+    /**
+     * Main Class
+     * @param args 
+     */
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        Formulario ventana = new Formulario();
+        
+        ventana.setVisible(true);
+    }
+}
